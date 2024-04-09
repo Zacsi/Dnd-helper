2191b14bc94c152d7046c1dd2d9a2b04dd21c6d1
Zacsi <matyi19991021@gmail.com>
2024-04-09 19:10:34 +0200

    Characters in new page

--- src/app/pages/karakterek/karakterek.component.ts
+++ src/app/pages/karakterek/karakterek.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-karakterek',
   templateUrl: './karakterek.component.html',
   styleUrls: ['./karakterek.component.scss']
 })
-export class KarakterekComponent {
+export class KarakterekComponent implements OnInit {
+  characters: Observable<any[]> = EMPTY; // EMPTY is an observable that completes immediately
 
+  constructor(private firestore: AngularFirestore) {}
+
+  ngOnInit() {
+    this.characters = this.firestore.collection('characters').valueChanges();
+  }
 }
