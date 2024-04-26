96e795c7885c7b4c3762765f2c3fb3f7471e513a
Matyas Sotonyi <matyas.sotonyi.ext@evosoft.com>
2024-04-26 13:02:42 +0200

    Frontend

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,32 +9,34 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  
-  email= new FormControl('');
-  password= new FormControl('');
-
+  email = new FormControl('');
+  password = new FormControl('');
+  errorMessage: string = ''; // Store error messages
 
   constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
-  login(){
-
-    if(this.email.value && this.password.value){
-      this.authService.login(this.email.value, this.password.value).then(cred =>{
+  login() {
+    if (this.email.value && this.password.value) {
+      this.authService.login(this.email.value, this.password.value).then(cred => {
         console.log(cred);
-        this.router.navigateByUrl('/main')
-      }).catch(error =>{
+        this.router.navigateByUrl('/main');
+      }).catch(error => {
+        this.errorMessage = 'Authentication failed: ' + error.message; // Set error message
         console.error(error);
       });
+    } else {
+      this.errorMessage = 'Please enter both email and password.'; // Validation message
     }
   }
+
   signOut() {
     this.authService.signOut().then(() => {
       this.router.navigateByUrl('/login');
     }).catch(error => {
       console.error('Failed to sign out:', error);
     });
-}
+  }
 }
